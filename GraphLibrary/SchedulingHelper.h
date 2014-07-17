ff1317fe6ab8c9d4f0a7557fd9e07c331de18899
szekizoli <szekizoli@gmail.com>
2014-07-17 14:10:41 +0200

    really add scheduling helper

---
+++ GraphLibrary/SchedulingHelper.h
@@ -0,0 +1,45 @@
+/**
+  *
+  * Date: 2014.07.17 13:21
+  * Author: Zoltan
+  */
+#ifndef GRAPHLIB_SCHEDULINGHELPER_H
+#define GRAPHLIB_SCHEDULINGHELPER_H
+
+#include <vector>
+
+#include "Options.h"
+#include "Solution.h"
+
+namespace msonlab {
+	namespace scheduling {
+
+		using std::vector;
+
+		class SchedulingHelper {
+			static unsigned int doComputeLengthSTAndRT(Solution::csPtr solution, Options::oPtr options,
+			vector<unsigned>& ST, vector<unsigned>& RT);
+		public:
+			///
+			/// Computes the length of the solution with the given option.
+			///
+			/// @param solution the result to use
+			/// @param options the options to use
+			/// @return the length
+			static unsigned int computeLength(Solution::csPtr solution, const Options::oPtr options);
+
+			static unsigned int computeLengthAndST(Solution::csPtr solution, const Options::oPtr options,
+				vector<unsigned>& ST);
+
+			static unsigned int computeLengthAndRT(Solution::csPtr solution, const Options::oPtr options,
+				vector<unsigned>& RT);
+
+			static unsigned int computeLengthSTAndRT(Solution::csPtr solution, const Options::oPtr options,
+				vector<unsigned>& ST, vector<unsigned>& RT);
+
+			static unsigned int computeLengthAndReuseIdleTime(Solution::sPtr& solution, const Options::oPtr& options);
+		};
+	}
+}
+
+#endif
