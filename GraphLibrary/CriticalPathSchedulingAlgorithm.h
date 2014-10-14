95300d5189e3c5a362cb9b1cc22f37c428f7853d
szekizoli <szekizoli@gmail.com>
2014-10-14 21:20:53 +0100

    correct typo

--- GraphLibrary/CriticalPathSchedulingAlgorithm.h
+++ GraphLibrary/CriticalPathSchedulingAlgorithm.h
@@ -10,10 +10,10 @@ namespace msonlab {
 
 		class CriticalPathSchedulingAlgorithm : public ListSchedulingAlgorithm
 		{
+			static CriticalPathSchedulingAlgorithm example;
 		protected:
 			virtual void determineCosts(const lw::lwgraph &graph, vector<unsigned>& costs) const;
 		public:
-			static CriticalPathSchedulingAlgorithm example;
 			CriticalPathSchedulingAlgorithm() = default;
 			CriticalPathSchedulingAlgorithm(examplar e) { SchedulingAlgorithm::add_scheduling_algorithm(this); }
 			virtual SchedulingAlgorithm::ptr build(OptionsPtr) const;
