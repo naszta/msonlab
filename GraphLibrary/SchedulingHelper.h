d1a924d89830bccd65ce736e273c679d8405a598
Zoltan Szekeres <szekizoli@gmail.com>
2015-05-01 00:03:57 +0200

    Fix indentation

--- GraphLibrary/SchedulingHelper.h
+++ GraphLibrary/SchedulingHelper.h
@@ -11,41 +11,39 @@
 #include "Options.h"
 #include "Solution.h"
 
-namespace msonlab {
-	namespace scheduling {
-		using std::vector;
+namespace msonlab { namespace scheduling {
+	using std::vector;
 
-		///
-		/// Computes the length of the solution with the given option.
-		///
-		/// @param solution the result to use
-		/// @param options the options to use
-		/// @return the length
-		template<typename SolutionType>
-		unsigned computeLength(const SolutionType &solution, const Options &options);
+	///
+	/// Computes the length of the solution with the given option.
+	///
+	/// @param solution the result to use
+	/// @param options the options to use
+	/// @return the length
+	template<typename SolutionType>
+	unsigned computeLength(const SolutionType &solution, const Options &options);
 		
-		template<typename SolutionType>
-		unsigned computeLengthAndST(const SolutionType &solution, const Options &options,
-			vector<unsigned>& ST);
+	template<typename SolutionType>
+	unsigned computeLengthAndST(const SolutionType &solution, const Options &options,
+		vector<unsigned>& ST);
 
-		template<typename SolutionType>
-		unsigned computeLengthAndRT(const SolutionType &solution, const Options &options,
-			vector<unsigned>& RT);
+	template<typename SolutionType>
+	unsigned computeLengthAndRT(const SolutionType &solution, const Options &options,
+		vector<unsigned>& RT);
 
-		template<typename SolutionType>
-		unsigned computeLengthSTAndRT(const SolutionType &solution, const Options &options,
-			vector<unsigned>& ST, vector<unsigned>& RT);
+	template<typename SolutionType>
+	unsigned computeLengthSTAndRT(const SolutionType &solution, const Options &options,
+		vector<unsigned>& ST, vector<unsigned>& RT);
 
-		template<typename SolutionType>
-		unsigned computeLengthAndReuseIdleTime(SolutionType& solution, const Options& options);
+	template<typename SolutionType>
+	unsigned computeLengthAndReuseIdleTime(SolutionType& solution, const Options& options);
 
-		template<typename SolutionType>
-		bool is_correct(const SolutionType &sol);
+	template<typename SolutionType>
+	bool is_correct(const SolutionType &sol);
 
-		template<typename SolutionType>
-		void write_solution(const SolutionType &solution, const Options& options, std::ostream& os);
-	}
-}
+	template<typename SolutionType>
+	void write_solution(const SolutionType &solution, const Options& options, std::ostream& os);
+}}
 
 #include "SchedulingHelper.cpp"
 
